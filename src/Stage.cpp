e7bc9d12e8698c0ab766e67d4f1f6893a54ba251
syunwi2 <syunwi2@gmail.com>
2021-11-17 21:41:13 +0900

    탈출구 동작 및 게임 클리어 구현

--- src/Stage.cpp
+++ src/Stage.cpp
@@ -1,8 +1,11 @@
 #include "Stage.h"
 
-Stage::Stage(ScenePtr bg, ScenePtr fg) {
-	background = bg;
-	frontground = fg;
+Stage::Stage() {
+	// 게임 타이틀 및 스테이지 별 스크립트를 보이기 위한 방탈 장면
+	frontground = Scene::create("시작 화면", CombatResource::BACKGROUND1);
+
+	// 게임이 실행되는 방탈 장면
+	background = Scene::create("배경", BoardResource::BACKGROUND);
 
 	// 지뢰찾기 보드 생성
 	board = new Board(background);
@@ -16,8 +19,22 @@ Stage::Stage(ScenePtr bg, ScenePtr fg) {
 		return true;
 	});
 
+	// background에서 보여줄 탈출하기 버튼
+	// 주어진 조건을 만족할 때만 보이기 위해 숨긴다.
+	escapeButton = Object::create(CellResource::EMPTY, background, 0, 690);
+	escapeButton->setOnMouseCallback([&](auto object, int x, int y, auto action)->bool {
+		board->setBoardStatus(BoardStatus::Clear);
+		escapeButton->hide();
+		return true;
+		});
+	escapeButton->hide();
+
 	// 스테이지의 이벤트 핸들러가 동작한다.
 	EventHandler();
+}
+
+ScenePtr Stage::getFrontground() {
+	return frontground;
 }
 
 void Stage::EventHandler() {
@@ -25,6 +42,12 @@ void Stage::EventHandler() {
 	boardStatusChecker = Timer::create(REFRESH_TIME);
 	boardStatusChecker->setOnTimerCallback([&](auto)->bool {
 		// Playing -> 이벤트 없음
+		// Escape -> 탈출하기 버튼 보이기
+		if (board->getBoardStatus() == BoardStatus::Escape) {
+			board->setBoardStatus(BoardStatus::Playing);
+			escapeButton->show();
+			showMessage("탈출구를 찾았습니다! 스테이지를 벗어나고 싶으면 탈출하기 버튼을 누르세요.\n지금 탈출하지 않고 남아있는 아이템을 획득하기 위해 게임을 계속 진행할 수 있습니다.");
+		}
 		// Claer -> 다음 스테이지
 		if (board->getBoardStatus() == BoardStatus::Clear) {
 			NextStage();
@@ -32,7 +55,7 @@ void Stage::EventHandler() {
 		// GameOver -> 보드 초기화?
 		if (board->getBoardStatus() == BoardStatus::GameOver) {
 			// ** 디버그용 ** 게임 종료
-			std::cout << "게임 오버!";
+			std::cout << "Game over!";
 			endGame();
 		}
 		boardStatusChecker->set(REFRESH_TIME);
@@ -44,18 +67,30 @@ void Stage::EventHandler() {
 }
 
 void Stage::NextStage() {
-	// 다음 스테이지로 넘어갈 때 보여줄 스크립트를 갱신한다.
-	// script->setImage();
-
-	// 스크립트를 가지고 있는 front 장면으로 입장한다.
-	frontground->enter();
-
 	// 스테이지 레벨을 갱신한다.
 	currentStageLevel++;
 
-	// 다음 지뢰찾기 보드로 갱신한다.
-	int row = board->getRow();
-	int col = board->getCol();
-	board->RefreshBoard(row + 2, col + 4);
+	// 정해진 스테이지를 모두 클리어했다면 게임을 클리어한 것이다.
+	if (currentStageLevel == STAGE_COUNT) {
+		// ** 디버그용 ** 게임 종료
+		std::cout << "Game Clear!";
+		endGame();
+	}
+
+	// 아직 클리어할 스테이지가 남았다면 게임을 진행한다.
+	else {
+		// 다음 스테이지로 넘어갈 때 보여줄 스크립트를 갱신한다.
+		// script->setImage();
+
+		// 스크립트를 가지고 있는 front 장면으로 입장한다.
+		frontground->enter();
+
+		// 다음 지뢰찾기 보드로 갱신한다.
+		int row = board->getRow();
+		int col = board->getCol();
+		board->RefreshBoard(row + 2, col + 4);
+
+		// 다음 지뢰찾기 보드의 배경을 갱신한다.
+	}
 }
 
